509f9d6027339e04cfff6ce03172e724af3a72b0
Mostafa Nazier <mustafanazieer@gmail.com>
2025-08-31 12:30:22 +0200

    pushing updates from main site to orbit app

---
+++ orbit/orbit/doctype/project_agreement/test_project_agreement.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Smart Vision Group and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestProjectAgreement(FrappeTestCase):
+	pass
